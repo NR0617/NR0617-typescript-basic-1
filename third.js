479d56e03859d37f168068cefe27c095054f08b0
NR0617 <nara20210617@gmail.com>
2023-01-15 18:41:29 +0900

    fix: make third file

--- third.js
+++ third.js
@@ -16,6 +16,7 @@ const human = {
         return "abc";
     },
 };
+//cf. declare let c: number;도 가능하다 외부에서 만들어진 애들을 타입선언 할 때 declare를 사용하는 것
 let target = [];
 forEach([1, 2, 3], (el) => target.push(el));
 forEach([1, 2, 3], (el) => {
@@ -27,3 +28,13 @@ const aa = {
     },
 };
 const bb = aa.talk(); //-> <unknown><number>로 하면 react가 잘 모르니까 as로 쓰세요
+// any를 쓸 바에 unknown을 쓰자 -> any는 타입 추론을 포기하는 것, unknown을 이후에 직접 타입을 추론해주는 것
+const cc = aa.talk();
+cc.talk();
+//unknown이 주로 사용되는 부분 -> try catch
+try {
+}
+catch (error) {
+    error.message;
+    //(error as AxiosError)
+}
