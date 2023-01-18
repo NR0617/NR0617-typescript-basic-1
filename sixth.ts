f503cd3d7e433d6d2b855636b4f34203ea162b46
NR0617 <nara20210617@gmail.com>
2023-01-18 18:16:17 +0900

    :sparkles: feat: sixth 만들기

--- sixth.ts
+++ sixth.ts
@@ -9,12 +9,16 @@ bcd(1, 2);
 bcd(1, 2, 3);
 bcd(1, 2, 3, 4);
 
-//제네릭 사용하는 방법
-function add<T>(x: T, y: T): T {
+//제네릭: 지금 타입은 모르겠는데 나중에 정하고 싶을 때 사용하는 것
+function add<T extends number | string, K extends string>(x: T, y: K): T {
     return x + y;
 }
-add<number>(1, 2);
+//add<number>(1, 2);
 add(1, 2);
-add<string>("1", "2");
+//add<string>("1", "2");
 add("1", "2");
 add(1, "2");
+
+//리액트에서 제네릭 기본값 주는 방법
+const addd = <T = unknown>(x: T, y: T) => ({ x, y });
+//const addd  = <T extends unknown>(x: T, y: T) => ({x, y})
